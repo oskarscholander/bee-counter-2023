c1a6c9e30b227188d4c4e20dcc597ec36ee9cad3
Oskar Schölander <oskar.scholander@nentgroup.com>
2023-06-09 11:55:06 +0200

    Rewrote stuff

--- count.ino
+++ count.ino
@@ -1,69 +1,63 @@
 #include <light_CD74HC4067.h>
 
 CD74HC4067 mux(8, 9, 10, 11);
-const int signal_pin = A1; // Pin A0 - Connected to Sig pin of CD74HC4067
+const int signal_pin_mux_1 = A1;
+const int signal_pin_mux_2 = A0;
 
-int INS = 0;
-int OUTS = 0;
+int ins = 0;
+int outs = 0;
 
-int analogReading;
+int count = 0;
+int last_count = 0;
 
-bool GATE_1_A;
-bool GATE_1_B;
+int i = 0;
+int channel;
 
-bool LAST_GATE_1_A;
-bool LAST_GATE_1_B;
+byte last_a;
+byte last_b;
 
-int COUNT = 0;
-int LAST_COUNT = 0;
+byte gate_1_a_sensor;
+byte gate_1_b_sensor;
+
+byte gate_1_a_byte;
+byte gate_1_b_byte;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(signal_pin, INPUT);
-}
-
-bool getSensorStatus(int analogReading) {
-  return analogReading >= 1020 ? false : true;
+  pinMode(signal_pin_mux_1, INPUT);
 }
 
 void loop() {
-  /**
-   * GATE 1 - First sensor status
-   */
-  mux.channel(0);
-  int GATE_1_A = getSensorStatus(analogRead(signal_pin));
+  gate_1_a_sensor = getSensorStatus(0);
+  gate_1_b_sensor = getSensorStatus(1);
 
-  /**
-   * GATE 1 - Second sensor status
-   */
-  mux.channel(1);
-  int GATE_1_B = getSensorStatus(analogRead(signal_pin));
+  last_a = gate_1_a_byte&0x1;
+  last_b = gate_1_b_byte&0x1;
+  int validated_state = abs(last_a - gate_1_a_sensor) + abs(last_b - gate_1_b_sensor);
 
-  // First sensor status changed and is active together with second sensor
-  if ((LAST_GATE_1_A != GATE_1_A) && (GATE_1_A && GATE_1_B)) {
-    OUTS++;
+  if (validated_state == 1) {
+    gate_1_a_byte = gate_1_a_byte<<1|gate_1_a_sensor;
+    gate_1_b_byte = gate_1_b_byte<<1|gate_1_b_sensor;
+
+    int a = gate_1_a_byte&0x1f;
+    int b = gate_1_b_byte&0x1f;
+    if (a == 6 && b == 12) { outs++; }
+    if (b == 6 && a == 12) { ins++; }
+
+    count = ins + outs;
+
+    if (last_count != count) {
+      Serial.print("In:  ");
+      Serial.println(ins);
+      Serial.print("Out:  ");
+      Serial.println(outs);
+      last_count = count;
+    }
   }
+}
 
-  // Second sensor status changed and is active together with first sensor
-  if ((LAST_GATE_1_B != GATE_1_B) && (GATE_1_B && GATE_1_A)) {
-    INS++;
-  }
-
-  // Save sensor status for next loop iteration
-  LAST_GATE_1_A = GATE_1_A;
-  LAST_GATE_1_B = GATE_1_B;
-
-  // Counted bees
-  COUNT = INS + OUTS;
-
-  // Short delay to give the bees some breating room
-  delay(50);
-
-  if (LAST_COUNT != COUNT) {
-    Serial.print("In:  ");
-    Serial.println(INS);
-    Serial.print("Out:  ");
-    Serial.println(OUTS);
-    LAST_COUNT = COUNT;
-  }
+byte getSensorStatus(int channel) {
+  mux.channel(channel);
+  delay(30);
+  return analogRead(signal_pin_mux_1) >= 1020 ? 0 : 1;
 }
